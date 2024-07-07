82d0af101d17e2a6cb20e8da0ac4562b867b7091
symfonyCDV <maciek.zielinski1@gmail.com>
2024-07-07 14:44:05 +0200

    Siema

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -163,4 +163,5 @@ class BlogController extends AbstractController
 
 
 
+
 }
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType; // Import the SubmitType
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,8 +19,18 @@ class ArticleType extends AbstractType
         $builder
             ->add('title', TextType::class)
             ->add('content', TextareaType::class, ['attr' => ['rows' => 8]])
+            ->add('created', DateTimeType::class, [
+                'widget' => 'single_text',
+                'html5' => false,
+                'format' => 'dd.MM.yyyy HH:mm',
+                'attr' => ['class' => 'js-datepicker'],
+            ])
             ->add('imageFile', FileType::class, [
-                'required' => false, // Jeśli pole nie jest wymagane
+                'required' => false,
+            ])
+            ->add('save', SubmitType::class, [ // Use SubmitType to add a submit button
+                'label' => 'Dodaj artykuł', // Button label
+                'attr' => ['class' => 'btn btn-primary'], // Optional CSS classes for styling
             ]);
     }
 
